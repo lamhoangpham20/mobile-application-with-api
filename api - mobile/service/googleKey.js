fea3f84ce8357064624bc95c7e6aab808e684d20
Lam Pham <t8phho00@students.oamk.fi>
2020-02-12 12:58:03 +0200

    delete key

--- api - mobile/service/googleKey.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: "497323396044-6cl7m07llg2etuqngq7jmkvafsgosbif.apps.googleusercontent.com",
-        clientSecret: "vZLKtzqIfknpcTBGxoj5dtdE"
-    }
-}
