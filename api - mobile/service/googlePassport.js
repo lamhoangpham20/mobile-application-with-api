56de952d8b5a6d72f308241cfb0564cb74243437
Lam Pham <t8phho00@students.oamk.fi>
2020-02-12 12:54:14 +0200

    delete key from repository and add oauth user to db

--- api - mobile/service/googlePassport.js
+++ api - mobile/service/googlePassport.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./googleKey');
-
+const db = require('../db');
 passport.use(
     new GoogleStrategy({
         // options for google strategy
@@ -10,6 +10,25 @@ passport.use(
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
-        console.log(profile);
+        db.query('SELECT * FROM users where idGoogle = ?', [profile.id])
+            .then(function (result) {
+                //check if user in db
+                if (result.length != 0) {
+                    // user already in the db
+                    console.log('user already in the db');
+                }
+                else {
+                    //create new user
+                    db.query('INSERT INTO users (idGoogle, name) VALUES (?,?)', [profile.id, profile.displayName])
+                        .then(results => {
+                            console.log(results);
+                            res.sendStatus(201);
+                        }).catch(err => { console.log(err); })
+                }
+            }).catch((err) => {
+                console.log(err);
+            }
+            )
+        // console.log(profile);
     })
 );
